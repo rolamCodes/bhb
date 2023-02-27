e798851c9c027bf3154e38c60dd992102548f91c
rolam <rholem.abdellah@gmail.com>
2023-02-27 20:36:44 +0100

    Nav styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import contact from '../assets/contact.png';
+import menu from '../assets/menu.png';
+import logo from '../assets/logo.png';
 
 function Nav() {
     return (
         <div className='nav-container'>
-            <button>Contact</button>
-            <img src='' alt='' />
-            <button>Menu</button>
+            <button><img className='nav-icon' src={contact} width={25} alt='' /></button>
+            <img className='nav-icon' src={logo} width={32} alt='' />
+            <button><img className='nav-icon' src={menu} width={25} alt='' /></button>
         </div>
     );
 }
