74bea6c7fbfa39823840c1d2565fbe9ebdbfc522
rolam <rholem.abdellah@gmail.com>
2023-03-01 13:39:30 +0100

    Minor tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
   const handleSignout = () => {
     signOut(auth)
       .then(() => {
-        //signed out
+        setSignedInUser(null);
       })
       .catch(err => console.log(err));
   }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import contact from '../assets/contact.png';
+import signout from '../assets/signout.png';
 import logo from '../assets/logo.png';
 import close from '../assets/close.png';
 import add from '../assets/add.png';
@@ -7,9 +7,8 @@ import add from '../assets/add.png';
 function Nav({ onAdd, onClose, isHabitOpen, onSignout }) {
     return (
         <div className='nav-container'>
-            <button><img className='nav-icon' src={contact} alt='contact us icon' /></button>
+            <button onClick={onSignout}><img className='nav-icon' src={signout} alt='sign out icon' /></button>
             <img className='logo' src={logo} alt='bad habit breaker logo' />
-            <button onClick={onSignout}>Sign out</button>
             <button onClick={isHabitOpen ? onClose : onAdd}>
                 {isHabitOpen ?
                     <img className='nav-icon' src={close} alt='close icon' />
